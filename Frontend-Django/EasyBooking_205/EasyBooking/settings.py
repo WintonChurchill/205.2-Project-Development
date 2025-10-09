efe5ad8b6760fe179ead8cced91e140623e73bb4
FloT28 <Florence.Taele@gmail.com>
2025-10-10 01:10:59 +1300

    updated files

--- Frontend-Django/EasyBooking_205/EasyBooking/settings.py
+++ Frontend-Django/EasyBooking_205/EasyBooking/settings.py
@@ -73,10 +73,7 @@ WSGI_APPLICATION = 'EasyBooking_205.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+#Enter postgreSQL database!
 }
 
 
