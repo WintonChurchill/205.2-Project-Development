0cade7fc6bfccf3bfad05a003db98439ee26fcce
FloT28 <Florence.Taele@gmail.com>
2025-11-02 22:42:11 +1300

    updated files for forgot password feature (in progress)

--- Frontend-Django/BookingSystem205/App/views.py
+++ Frontend-Django/BookingSystem205/App/views.py
@@ -2,6 +2,17 @@ from django.shortcuts import render
 from django. template import loader 
 from django.http import HttpResponse
 
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User 
+from django.contrib.auth import authenticate, login, logout 
+from django.contrib.auth.decorators import login_required 
+from django.contrib import messages 
+from django.conf import settings 
+from django.core.mail import EmailMessage 
+from django.utils import timezone 
+from django.urls import reverse 
+from .models import * 
+
 #extra django libraries to complete forgot password / reset password feature ! 
 from django.shortcuts import render, redirect 
 from django.contrib.auth.models import User 
@@ -9,6 +20,8 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout 
 from django.core.mail import send_mail
 from django.contrib.auth.decorators import login_required
+
+from django.views import View
 
 # Create your views here.
 def home(request):
@@ -20,35 +33,35 @@ def contact(request):
 def LoginView(request): 
     return render(request, 'Login.html')
 
-#Work in progress 31/10/25 - 02/10/25
+
 def RegisterView(request): 
-    #Incoming form submission collect user data 
-    if request.method == 'POST': 
+    if request.method =='POST': 
+        #collect user inputs 
         email = request.POST.get('email')
         password = request.POST.get('password')
 
         user_data_has_error = False 
 
-    #Email Validation 
-    if User.objects.filter(email=email).exists(): 
-        user_data_has_error = True 
-        messages.error(request, 'Email already exists')
-    
-    #Password Validation at least 8 characters long! 
-    if len(password) < 8: 
-        user_data_has_error = True 
-        messages.error(request, 'Password must be at least 8 characters long!')
+        #check if email not being used
+        if User.objects.filter(email=email).exists(): 
+            user_data_has_error = True 
+            messages.error(request, 'Email already exists')
 
-    #Create new user if no error and redirect to the Login page: 
-    if not user_data_has_error: 
-        new_user = User.objects.create_user(
-            email = email, 
-            password = password, 
-        )
-        messages.sucess(request, 'Account created. You can now Login')
-        return redirect('Login')
-    else: 
-        return redirect('Register')
+        #password validation 
+        if len(password) < 8: 
+            user_data_has_error = True 
+            messages.error(request, 'Password must be at least 8 characters long')
+
+        if not user_data_has_error: 
+            new_user = User.objects.create_user(
+                email = email, 
+                password = password
+            )
+            messages.success(request, 'Account created. Login Now!')
+            return redirect('Login.html')
+        else: 
+            return HttpResponse(request, 'Register.html')
+
     
 
 def Contact_form(request): 
