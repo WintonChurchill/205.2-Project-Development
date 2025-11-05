f7de91e05263c8f65eaca69c7e81d16d7343f214
FloT28 <Florence.Taele@gmail.com>
2025-11-05 23:36:15 +1300

    update progress for forgot password feature (functional nearly complete) program still working

--- Frontend-Django/BookingSystem205/App/views.py
+++ Frontend-Django/BookingSystem205/App/views.py
@@ -2,17 +2,6 @@ from django.shortcuts import render
 from django. template import loader 
 from django.http import HttpResponse
 
-from django.shortcuts import render, redirect
-from django.contrib.auth.models import User 
-from django.contrib.auth import authenticate, login, logout 
-from django.contrib.auth.decorators import login_required 
-from django.contrib import messages 
-from django.conf import settings 
-from django.core.mail import EmailMessage 
-from django.utils import timezone 
-from django.urls import reverse 
-from .models import * 
-
 #extra django libraries to complete forgot password / reset password feature ! 
 from django.shortcuts import render, redirect 
 from django.contrib.auth.models import User 
@@ -21,7 +10,9 @@ from django.contrib.auth import authenticate, login, logout
 from django.core.mail import send_mail
 from django.contrib.auth.decorators import login_required
 
-from django.views import View
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy  
+from django.views.generic import CreateView
 
 # Create your views here.
 def home(request):
@@ -30,43 +21,52 @@ def home(request):
 def contact(request): 
     return render(request, 'contact.html')
 
+def signupcopy(request): 
+    return redirect(request, 'signup copy.html')
+
+#In-progress (03/11/25)
 def LoginView(request): 
-    return render(request, 'Login.html')
-
-
-def RegisterView(request): 
-    if request.method =='POST': 
-        #collect user inputs 
+    if request.method == 'POST': 
+        #collect user input from front-end
         email = request.POST.get('email')
         password = request.POST.get('password')
 
-        user_data_has_error = False 
+        user = authenticate(request, email=email, password=password)
 
-        #check if email not being used
-        if User.objects.filter(email=email).exists(): 
-            user_data_has_error = True 
-            messages.error(request, 'Email already exists')
+        if user is not None: 
+            login(request, user)
 
-        #password validation 
-        if len(password) < 8: 
-            user_data_has_error = True 
-            messages.error(request, 'Password must be at least 8 characters long')
-
-        if user_data_has_error: 
-            return redirect('register')
-        else:
-            new_user = User.objects.create_user(
-                email = email, 
-                password = password
-            )
-            messages.success(request, 'Account created. Login Now!')
-            return redirect('login')
-    return render(request, 'register.html')
+            return redirect('Home')
     
+        else: 
+            messages.error(request, "Invalid login credentials")
+            return redirect('login')
 
+    return render(request, 'Login.html')
+
+
+#Incomplete
+def LogoutView(request): 
+    logout(request)
+    return redirect('login')
+
+#Incomplete
+def ForgotPassword(request): 
+    return render(request, 'forgot_password.html')
+
+#Incomplete
+def PasswordResetSent(request, reset_id): 
+    return redirect('fogot-password')
+
+#Incomplete
+def ResetPassword(request, reset_id): 
+    return redirect('reset-password', reset_id=reset_id)
+
+#Complete
 def Contact_form(request): 
     return render(request, 'Contact_form.html')
 
+#Complete
 def about(request): 
     return render(request, 'about.html')
 
