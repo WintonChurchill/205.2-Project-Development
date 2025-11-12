661081801e08e7bd3e768bb71f07fecbc97b748b
FloT28 <Florence.Taele@gmail.com>
2025-11-13 00:09:45 +1300

    Forgot Password - new files added (full code completed outside of this project to avoid any error conflicts) *will be completed copying tomorrow 13/11/25

--- Frontend-Django/BookingSystem205/App/views.py
+++ Frontend-Django/BookingSystem205/App/views.py
@@ -13,10 +13,87 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy  
 from django.views.generic import CreateView
+from django.contrib import messages
+from django.shortcuts import render, request
+
+#Clean up files as well / commenting etc..
 
 # Create your views here.
+@login_required
 def home(request):
     return render(request, 'home.html')
+
+#IMPORTANT NOTICE: due to I had tested our project via a new register system outside of this project, 
+#I will be completing the rest of the copying of the new files, modifying and completed by tomorrow during class time 
+#Only part left is to test everything in new project and also forgot password feature 
+#will provide screenshots proof work completed for testin on 12/11/25
+
+#new Register page - working (edit and final test tomorrow during class time!)
+def RegisterView(request): 
+    if request.method == 'POST': 
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        #Flag Error 
+        user_data_has_error = False 
+
+        #Username validation check if exists or Not
+        if User.objects.filter(username=username).exists():
+            user_data_has_error = True 
+            messages.error(request, 'Username already exists')
+
+        #Email validation check if exists or Not
+        if User.objects.filter(email=email).exists():
+            user_data_has_error = True 
+            messages.error(request, 'Email already exists')
+
+        #Password validation
+        if len(password) < 5: 
+            user_data_has_error = True
+            messages.error(request, 'Password must be at least 5 characters')
+
+        #create a new user if user input is correct! 
+        if not user_data_has_error:
+            new_user = User.objects.create_user(
+                username = username, 
+                email = email, 
+                password = password
+            )
+            messages.success(request, 'Account created succesfully!')
+            return redirect('login')
+        else: 
+            return redirect('register')
+
+    return render(request, 'register.html')
+
+    return render(request, 'register1.html')
+
+#new login page - working! (need to test tomorrow during class time)
+def LoginView(request): 
+    if request.method == "POST": 
+        username = request.POST.get("username")
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None: 
+            login(request, user)
+
+            return redirect('home')
+        else: 
+            messages.error(request, "Invalid login credentials")
+            return redirect('login')
+
+    return render(request, 'Login.html')
+
+#completed and working
+def LogoutView(request): 
+    logout(request)
+
+    return redirect('login')
+
+
 
 def signup(request): 
     return redirect(request, 'signup.html')
@@ -24,14 +101,13 @@ def signup(request):
 def contact(request): 
     return render(request, 'contact.html')
 
-#In-progress (03/11/25)
 def LoginView(request): 
     if request.method == 'POST': 
         #collect user input from front-end
-        email = request.POST.get('email')
+        username = request.POST.get('username')
         password = request.POST.get('password')
 
-        user = authenticate(request, email=email, password=password)
+        user = authenticate(request, username=username, password=password)
 
         if user is not None: 
             login(request, user)
--- Frontend-Django/BookingSystem205/accounts/views.py
+++ Frontend-Django/BookingSystem205/accounts/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy  
 from django.views.generic import CreateView
 from django_password_eye.fields import PasswordEye 
 from django_password_eye.widgets import PasswordEyeWidget
+from django.contrib.auth import update_session_auth_hash
+from django.contrib import messages
 
 #Register Form Views(1
 class SignUpView(CreateView): 
@@ -11,6 +13,7 @@ class SignUpView(CreateView):
    success_url = reverse_lazy('login')   
    template_name = "registration/signup.html" 
    
+
    #Add Email replacement of username and password 
 
 #Forgot Password View 
